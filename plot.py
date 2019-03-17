b89b6bf20197d31613ddfe6eaed79387ae99f276
Trystan May <trystanmay@gmail.com>
2019-03-16 19:44:24 -0500

    Initial Commit.
    Signed-off-by: Trystan May <trystanmay@gmail.com>

---
+++ plot.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+from simulation import get_chance_of_positions as get_chance
+
+
+def plot(n):
+	"""
+	Saves the plots of the chance of positions from simulation.py
+
+	:param n: the number of steps the simulation will take.
+	"""
+	num_of_iterations, counter = get_chance(n)
+
+	fig = plt.figure()
+
+	ax = fig.add_subplot(111)
+	ax.set_title("Position Color Map (n = {})".format(n))
+	ax.set_xlabel("Number of iterations: {}".format(int(num_of_iterations)))
+
+	plt.imshow(counter, cmap=plt.get_cmap('Blues_r'))
+
+	ax.set_aspect('equal')
+
+	divider = make_axes_locatable(ax)
+	cax = divider.append_axes("right", size="5%", pad=0.05)
+
+	plt.colorbar(orientation='vertical', cax=cax)
+	fig.savefig('plots/plot-{}.png'.format(n), dpi=fig.dpi)
+
+
+def main(max_n=10):
+	"""
+	The main function for this file. Makes max_n plots of the simulation.
+
+	:param max_n: The number of plots to generate (default 10)
+	"""
+	for n in range(1, max_n + 1):
+		plot(n)
+
+
+if __name__ == "__main__":
+	main()
---
+++ simulation.py
@@ -0,0 +1,84 @@
+"""
+A particle starts at (0, 0) and moves in one unit independent
+steps with equal probabilities of 1/4 in each of the four
+directions: north, south, east, and west. Let S equal
+the east-west position and T the north-south position
+after n steps.
+"""
+
+from random import random
+
+import numpy as np
+
+# Directions (K -> V is initial of direction -> (dx, dy)
+directions = {
+	'S': (0, -1),
+	'N': (0, 1),
+	'E': (1, 0),
+	'W': (-1, 0)
+	}
+
+
+def get_direction():
+	"""
+	Get a random direction. Each direction has a 25% chance of occurring.
+	
+	:returns: the chosen directions changes in x and y
+	"""
+	r = random()
+	if r < .25:
+		return directions['S']
+	elif r < .5:
+		return directions['N']
+	elif r < .75:
+		return directions['E']
+	return directions['W']
+
+
+def change_position(curr_pos, change_in_pos):
+	"""
+	Updates the current location based on the change in position.
+	
+	:returns: the update position (x, y)
+	"""
+	return curr_pos[0] + change_in_pos[0], curr_pos[1] + change_in_pos[1]
+
+
+def increment_counter(counter, end_pos):
+	"""
+	Increments the provided counter at the given location.
+	
+	:param counter: an numpy ndarray with the number of all ending locations in the simulation.
+	:param end_pos: the ending position of the last round of the simulation.
+	:returns: the updated counter.
+	"""
+	counter[end_pos[1], end_pos[0]] += 1
+	return counter
+
+
+def get_chance_of_positions(n):
+	"""
+	Gets the approximated chance the particle ends at a given location.
+	
+	Starting location is in the center of the output.
+	
+	:param n: The number of steps the simulation is to take.
+	:returns: the number of iterations and an ndarray with the approximated chance the particle would be at each location.
+	"""
+	# The starting position starts at n, n so that we can pass in the location
+	# into the counter without worrying about negative numbers.
+	starting_pos = (n, n)
+
+	total_num_of_sims = 10 ** 5
+
+	counter = np.zeros(shape=(2 * n + 1, 2 * n + 1))
+
+	for j in range(total_num_of_sims):
+		curr_pos = starting_pos
+		for i in range(n):
+			change_in_pos = get_direction()
+			curr_pos = change_position(curr_pos, change_in_pos)
+		counter = increment_counter(counter, curr_pos)
+
+	chances = np.round(counter / total_num_of_sims, decimals=n + 1)
+	return total_num_of_sims, chances
